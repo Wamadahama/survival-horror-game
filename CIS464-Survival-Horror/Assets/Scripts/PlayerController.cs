39b48cfabb73f6e5744d64029326a7b8e7f8fbb0
Wamadahama <smurftown2@gmail.com>
2019-11-29 18:29:36 -0500

    character renderer, started projectiles

--- CIS464-Survival-Horror/Assets/Scripts/PlayerController.cs
+++ CIS464-Survival-Horror/Assets/Scripts/PlayerController.cs
@@ -4,17 +4,67 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public int Health = 100; 
-
+    public int Health = 100;
+    public GameObject[] inventory;
+    public int selectedItem = 0; 
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void OnMouseDown()
+    {
+        SelectionHandler.ClearActiveSelections();
+
+        this.activeSelection = true; // This will be important to display an interface options pertaining to a specific unit when it's selected 
+        ToggleSprite();
+        ShowUnitStats();
+        //PrintSelectedObjects();
+    }
+
+    void spawnProjectile()
+    {
+
+    }
+
+    void switchItem()
+    {
+        if (selectedItem+1 > inventory.Length)
+        {
+            selectedItem = 0;
+        } else
+        {
+            selectedItem++; 
+        }
+    }
+
+    public float speed = 1f;
+    public Rigidbody2D rigidbody;
+    public CharacterRender renderer;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        renderer = GetComponentInChildren<CharacterRender>();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 current = rigidbody.position;
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        var target = (new Vector2(x, y) * speed);
+
+        Vector2 move = target * speed;
+        Vector2 next = current + target * Time.fixedDeltaTime;
+        renderer.SetSprite(move);
+        rigidbody.MovePosition(next);
+
     }
+
+
 }
--- CIS464-Survival-Horror/Assets/Scripts/PlayerMovement.cs
+++ CIS464-Survival-Horror/Assets/Scripts/PlayerMovement.cs
@@ -4,30 +4,5 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 1f;
-    public Rigidbody2D rigidbody;
-    public CharacterRender renderer; 
-    // Start is called before the first frame update
-    void Awake()
-    {
-        rigidbody = GetComponent<Rigidbody2D>();
-        renderer = GetComponentInChildren<CharacterRender>(); 
-   }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector2 current = rigidbody.position; 
-
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-
-        var target = (new Vector2(x, y) * speed);
-
-        Vector2 move = target * speed; 
-        Vector2 next = current + target * Time.fixedDeltaTime;
-        renderer.SetSprite(move); 
-        rigidbody.MovePosition(next); 
-
-    }
 }
