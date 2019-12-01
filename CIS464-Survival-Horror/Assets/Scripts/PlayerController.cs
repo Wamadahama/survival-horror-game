edb0f46dd13b73b99de20f76a4c05af47f4e7a88
Wamadahama <smurftown2@gmail.com>
2019-11-30 19:49:16 -0500

    fubar

--- CIS464-Survival-Horror/Assets/Scripts/PlayerController.cs
+++ CIS464-Survival-Horror/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     public int Health = 100;
     public GameObject[] inventory;
-    public int selectedItem = 0; 
+    public int selectedItem = 0;
+    public float speed = 1f;
+    public Rigidbody2D rigidbody;
+    public CharacterRender renderer;
+    public GameObject projectile; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +24,18 @@ public class PlayerController : MonoBehaviour
     }
 
     void spawnProjectile()
-    {
+    { 
+
+        Vector3 shootDirection;
+        shootDirection = Input.mousePosition;
+        shootDirection.z = 0.0f;
+        shootDirection = Camera.main.ScreenToWorldPoint(shootDirection);
+        shootDirection -= transform.position;
+
+
+        GameObject spellcast = Instantiate(projectile, transform.position, Quaternion.Euler(new Vector2(0, 0)));
+        var body = spellcast.GetComponent<Rigidbody2D>();
+        body.velocity = new Vector2(shootDirection.x * 5, shootDirection.y * 5); 
 
     }
 
@@ -34,9 +50,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    public float speed = 1f;
-    public Rigidbody2D rigidbody;
-    public CharacterRender renderer;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -47,6 +61,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            spawnProjectile();
+        }
+
         Vector2 current = rigidbody.position;
 
         float x = Input.GetAxis("Horizontal");
