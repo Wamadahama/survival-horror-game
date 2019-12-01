80125808f1110f18a75d69c3eff1845be5cceb82
divester529 <damonivester529@gmail.com>
2019-11-30 20:32:33 -0500

    I added the mob spawnah guysss I did ittt I did ittttt

---
+++ CIS464-Survival-Horror/Assets/MobSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject mobType;
+    [SerializeField] int quantity;
+    [SerializeField] float spawnRadius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void spawn()
+    {
+        Random rand = new Random();
+        Vector3 position = GetComponent<Transform>().position;
+
+        for (int i = 0; i < quantity; i++)
+        { 
+            Vector2 spawnLoc = new Vector2(position.x+Random.Range(-spawnRadius, spawnRadius), position.y+Random.Range(-spawnRadius, spawnRadius));
+
+            GameObject enemy = (GameObject)Instantiate(mobType, new Vector2(spawnLoc.x, spawnLoc.y), Quaternion.identity);
+        }
+    }
+}
